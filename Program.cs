4dbc13fbcf89634b8d7252fb5d345dacca21262f
ArasakiCaio <sasazaki08@gmail.com>
2026-03-04 21:47:16 -0300

    choire: conectando banco de dados

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Npgsql;
+
+// Get the connection string
+string connectionString = ConfigurationHelper.GetConnectionString("DefaultConnection");
+
+
+// Connect to the PostgreSQL server
+await using var conn = new NpgsqlConnection(connectionString);
+await conn.OpenAsync();
+
+Console.WriteLine($"The PostgreSQL version: {conn.PostgreSqlVersion}");
